684426c5bcaa286229f838d6a48b23fdc98b141b
kachette <54746037+kachette@users.noreply.github.com>
2020-11-03 19:29:47 +0000

    Added Checkout And the personal form

--- vue.js
+++ vue.js
@@ -3,24 +3,40 @@ var webstore = new Vue ({
     data: {
         showProduct: true,
         sitename: "Classes & Activities",
+
         product:{
             id:1001,
             title:"Maths Class",
             description:"An outstanding class" + " not taught by Gabriel",
             price:20,
-            image:"images/index.png",
+            image:"images/product.png",
             availableInventory: 5,
         },
-        cart:[]
+        order:{
+            firstName: '',
+            lastName: '',
+            address: '',
+            city: '',
+            zip: '',
+            state: '',
+            method: 'Home',
+            order: false,
+            sendGift: 'Send as a gift',
+            dontSendGift: 'Do not send as a gift'
+        },
+            cart:[]
     },
     methods: {
         addToCart: function() {
             this.cart.push( this.product.id)
-            
-            },
-            showCheckout() {
-                this.showProduct = this.showProduct ? false : true;
-        }
+        },
+
+        showCheckout() {
+            this.showProduct = this.showProduct ? false : true;
+        },
+
+        submitForm() {alert('Order Submitted !')}
+    
         },
         computed: {
         cartItemCount: function() {
